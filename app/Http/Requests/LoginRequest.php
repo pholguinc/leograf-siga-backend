c92c8ed00836335d03b831110b35fbe65da817ef
Pedro Holguin <holguinpedro90@gmail.com>
2024-05-24 15:28:03 -0500

    login fix

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -26,7 +26,6 @@ class LoginRequest extends BaseFormRequest
         return [
             'email' => 'required|email',
             'password' => 'required',
-            'captcha' => 'required',
         ];
     }
 
@@ -36,7 +35,6 @@ class LoginRequest extends BaseFormRequest
             'email.required' => 'El campo email es obligatorio.',
             'email.email' => 'Ingrese un correo válido',
             'password.required' => 'El campo contraseña es obligatorio.',
-            'captcha.required' => 'El campo captcha es obligatorio.',
 
         ];
     }
